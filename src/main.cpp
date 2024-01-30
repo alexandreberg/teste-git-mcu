91b4ca66525f752dedaa8e84f64dd182fa120eef
alexandreberg <alexandreberg@gmail.com>
2024-01-30 09:50:46 -0300

    chg src\main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+/*
+  US-100 Ultrasonic Sensor - Serial Mode
+  modified on 26 Sep 2020
+  by Mohammad Reza Akbari @ Electropeak
+  Home
+*/
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(19,18); //RX,TX
+
+unsigned int HighByte = 0;
+unsigned int LowByte  = 0;
+unsigned int Len  = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  Serial.begin(115200);
+  mySerial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  Serial.println("Lendo a distancia:");
+  mySerial.flush();
+  mySerial.write(0X55);                           // trig US-100 begin to measure the distance
+  delay(500);                                  
+  if (mySerial.available() >= 2)                  // check receive 2 bytes correctly
+  {
+    HighByte = mySerial.read();
+    LowByte  = mySerial.read();
+    Len  = HighByte * 256 + LowByte;          // Calculate the distance
+    if ((Len > 1) && (Len < 10000))
+    {
+      Serial.print("Distance: ");
+      Serial.print(Len, DEC);          
+      Serial.println("mm");                  
+    }
+  }
+  delay(1000);                                    
+} 
